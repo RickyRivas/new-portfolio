0a43e55810ae1e6f75f185ab7226b19262934b3e
Ricky Rivas <rickyrivas918@gmail.com>
2025-04-22 21:42:09 -0500

    base stuff & gsap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {});
+</script>
+
+<section id="hero">
+	<div class="hero__mod">
+		<h1>Welcome to SvelteKit</h1>
+		<p>
+			Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+		</p>
+	</div>
+</section>
